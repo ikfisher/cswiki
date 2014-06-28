c848c0b444e8faf046ebc5f6ed31934529ab26ec
Zhi <ikfisher@163.com>
2014-06-27 23:02:13 -0700

    syncdb

--- cswiki/settings.py
+++ cswiki/settings.py
@@ -30,21 +30,22 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+ #   'django.contrib.admin',
+ #   'django.contrib.auth',
+ #  'django.contrib.contenttypes',
+ #   'django.contrib.sessions',
+ #   'django.contrib.messages',
+ #   'django.contrib.staticfiles',
+     'dbtest',
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+   #  'django.contrib.sessions.middleware.SessionMiddleware',
+   #  'django.middleware.common.CommonMiddleware',
+   # 'django.middleware.csrf.CsrfViewMiddleware',
+   # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+   # 'django.contrib.messages.middleware.MessageMiddleware',
+   # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'cswiki.urls'
@@ -57,8 +58,11 @@ WSGI_APPLICATION = 'cswiki.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 't1',
+        'USER': 'liu',
+	'PASSWORD': '111',
+        'HOST': '127.0.0.1',
     }
 }
 
--- cswiki/urls.py
+++ cswiki/urls.py
@@ -1,12 +1,7 @@
 from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
+from cswiki import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'cswiki.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+  
+     url(r'^index/$',views.index),
 )
